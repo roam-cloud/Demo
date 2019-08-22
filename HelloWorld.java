cdb39a6528259910014511e4ca66a11b0122751f
roam-cloud <928307128@qq.com>
2019-08-22 15:27:18 +0800

    test b2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld{
       public static void main(String [] args){
-            System.out.println("1");
+            System.out.println("10");
             System.out.println("hello");
-            System.out.println("2");
+            System.out.println("20");
             System.out.println("hello");
        }
 }
